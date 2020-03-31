9d6e331913dbf46980ceb29a8d52682ca24d6862
teddy <william.k97430@gmail.com>
2020-03-31 17:01:29 +0200

    frist Activity style /recyclerview good

--- SurvieApp/app/src/main/java/com/learn/survieapp/activityfallow/MainActivity.java
+++ SurvieApp/app/src/main/java/com/learn/survieapp/activityfallow/MainActivity.java
@@ -1,16 +1,58 @@
 package com.learn.survieapp.activityfallow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import com.learn.survieapp.R;
+import com.learn.survieapp.adaptateurRecyclerView.RecyclerViewAdapted1;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> valeurRegion= new ArrayList<>();
+    ArrayList<Integer> valeurRegionImageview= new ArrayList<>();
+    ArrayList<String> valeurSurvie= new ArrayList<>();
+    ArrayList<String> valeurNourriture= new ArrayList<>();
+    ArrayList<String> valeurEau= new ArrayList<>();
+    ArrayList<String> valeurPlante = new ArrayList<>();
+    RecyclerViewAdapted1 recyclerViewAdapted1;
+    RecyclerView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data = findViewById(R.id.firstrecyclerview);
+
+        valeurRegion.add("Montagne");
+        valeurRegionImageview.add(R.drawable.regionmontagne);
+        valeurSurvie.add("1");
+        valeurNourriture.add("1");
+        valeurEau.add("1");
+        valeurPlante.add("1");
+        valeurRegion.add("Dessert");
+        valeurRegionImageview.add(R.drawable.regionmontagne);
+        valeurSurvie.add("1");
+        valeurNourriture.add("1");
+        valeurEau.add("1");
+        valeurPlante.add("1");
+
+
+        recyclerViewAdapted1 = new RecyclerViewAdapted1(this,  valeurRegion,
+                                                                    valeurRegionImageview,
+                                                                    valeurSurvie,
+                                                                    valeurNourriture,
+                                                                    valeurEau,
+                                                                    valeurPlante);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,1,GridLayoutManager.VERTICAL,false);
+        data.setLayoutManager(gridLayoutManager);
+        data.setAdapter(recyclerViewAdapted1);
     }
 }
--- SurvieApp/app/src/main/java/com/learn/survieapp/adaptateurRecyclerView/RecyclerViewAdapted1.java
+++ SurvieApp/app/src/main/java/com/learn/survieapp/adaptateurRecyclerView/RecyclerViewAdapted1.java
@@ -1,8 +1,87 @@
 package com.learn.survieapp.adaptateurRecyclerView;
 
-public class RecyclerViewAdapted1 {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.learn.survieapp.R;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapted1 extends RecyclerView.Adapter<RecyclerViewAdapted1.ViewHolder> {
+
+    ArrayList<String> valeurRegion;
+    ArrayList<Integer> valeurRegionImageview;
+    ArrayList<String> valeurSurvie;
+    ArrayList<String> valeurNourriture;
+    ArrayList<String> valeurEau;
+    ArrayList<String> valeurPlante;
+    LayoutInflater inflater;
+
+    public RecyclerViewAdapted1(Context ctx,
+                                ArrayList<String> valeurRegion,
+                                ArrayList<Integer> valeurRegionImageview,
+                                ArrayList<String> valeurSurvie,
+                                ArrayList<String> valeurNourriture,
+                                ArrayList<String> valeurEau,
+                                ArrayList<String> valeurPlante) {
+        this.valeurRegion = valeurRegion;
+        this.valeurRegionImageview = valeurRegionImageview;
+        this.valeurSurvie = valeurSurvie;
+        this.valeurNourriture = valeurNourriture;
+        this.valeurEau = valeurEau;
+        this.valeurPlante = valeurPlante;
+        this.inflater = LayoutInflater.from(ctx);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapted1.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.firstactivitycustum,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapted1.ViewHolder holder, int position) {
+        holder.textTitreRegion.setText(valeurRegion.get(position));
+        holder.valeurSurvie.setText(valeurSurvie.get(position));
+        holder.iconViewRegion.setImageResource(valeurRegionImageview.get(position));
+
+        //holder.valeurSurvie.setText(valeurSurvie.get(position));
+        holder.valeurNourriture.setText(valeurNourriture.get(position));
+        holder.valeurEau.setText(valeurEau.get(position));
+        holder.valeurPlante.setText(valeurPlante.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return valeurRegion.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView iconViewRegion;
+        TextView textTitreRegion;
+        TextView valeurSurvie;
+        TextView valeurEau;
+        TextView valeurNourriture;
+        TextView valeurPlante;
 
 
-
-
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            iconViewRegion = itemView.findViewById(R.id.imageregiondesurvie);
+            textTitreRegion = itemView.findViewById(R.id.texttyperegion);
+            valeurSurvie = itemView.findViewById(R.id.valeurchancesurvie);
+            valeurEau = itemView.findViewById(R.id.valeurchanceeau);
+            valeurPlante = itemView.findViewById(R.id.valeurchanceplante);
+            valeurNourriture = itemView.findViewById(R.id.valeurchancenourriture);
+        }
+    }
 }
